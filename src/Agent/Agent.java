c01c33ecca2d31b72c7f509070e6131d525b7163
Colton Trujillo <coltonbtrujillo@cs.unm.edu>
2019-11-15 20:35:09 -0700

    implementation: add all fields to Agent

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,7 +1,21 @@
 package Agent;
 
+import AuctionHouse.AuctionHouse;
+import Helper.Bid;
+import javafx.beans.property.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agent {
-    private int accountNumber;
-    private String name;
-    private double liquidFunds;
+    private int accountNumber = -1;
+    private SimpleStringProperty name = new SimpleStringProperty("Unknown Human");
+    private double liquidFunds = 0.00;
+    private DoubleProperty currentBalance = new SimpleDoubleProperty(0);
+    private DoubleProperty availableFunds = new SimpleDoubleProperty(0);
+    private ListProperty<Bid> bidsPlaced = new SimpleListProperty<>();
+    private ListProperty<AuctionHouse> availableHouses = new SimpleListProperty<>();
+    private AuctionHouse selectedHouse;
+    private StringProperty selectedHouseString = new SimpleStringProperty("NONE");
+
 }
