5bbd157abfe37aad1af12ba20773c0b2a4280441
Colton Trujillo <coltonbtrujillo@cs.unm.edu>
2019-11-19 20:47:52 -0700

    feat: Gui and Agent bound

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -9,13 +9,43 @@ import java.util.List;
 
 public class Agent {
     private int accountNumber = -1;
-    private SimpleStringProperty name = new SimpleStringProperty("Unknown Human");
     private double liquidFunds = 0.00;
-    private DoubleProperty currentBalance = new SimpleDoubleProperty(0);
-    private DoubleProperty availableFunds = new SimpleDoubleProperty(0);
+    private double currentBalance = 0;
+    private double availableFunds = 0;
     private ListProperty<Bid> bidsPlaced = new SimpleListProperty<>();
     private ListProperty<AuctionHouse> availableHouses = new SimpleListProperty<>();
     private AuctionHouse selectedHouse;
-    private StringProperty selectedHouseString = new SimpleStringProperty("NONE");
+    private StringProperty currentBalanceProperty = new SimpleStringProperty(String.valueOf(currentBalance));
+    private StringProperty availableFundsProperty = new SimpleStringProperty(String.valueOf(availableFunds));
+    private StringProperty selectedHouseProperty = new SimpleStringProperty("NONE");
+    private StringProperty name = new SimpleStringProperty("Unknown Human");
+    private StringProperty selectedItemProperty = new SimpleStringProperty("NONE");
 
+    public StringProperty getSelectedItemProperty() {
+        return selectedItemProperty;
+    }
+
+    public ListProperty<Bid> getBidsPlaced() {
+        return bidsPlaced;
+    }
+
+    public ListProperty<AuctionHouse> getAvailableHouses() {
+        return availableHouses;
+    }
+
+    public StringProperty getCurrentBalanceProperty() {
+        return currentBalanceProperty;
+    }
+
+    public StringProperty getAvailableFundsProperty() {
+        return availableFundsProperty;
+    }
+
+    public StringProperty getSelectedHouseProperty() {
+        return selectedHouseProperty;
+    }
+
+    public StringProperty getName() {
+        return name;
+    }
 }
