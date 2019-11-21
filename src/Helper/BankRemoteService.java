197fad3b092f9d7c3dc37e6ca2ae6b38e42d6555
Marcus Trujillo <marcus.t.trujillo@gmail.com>
2019-11-21 12:55:55 -0700

    fix: id vs name debate

--- src/Helper/BankRemoteService.java
+++ src/Helper/BankRemoteService.java
@@ -10,8 +10,8 @@ import java.util.List;
 public interface BankRemoteService extends java.rmi.Remote{
     //here we define what the client can call remotely
     public boolean sufficientFunds(int accountNumber, int amountNeeded) throws RemoteException;
-    public int registerAgent(int id, int initialBalance) throws RemoteException; // for Agents
-    public int registerAuctionHouse(int id) throws RemoteException; //this is for AuctionHouse, intial balance is always 0
+    public int registerAgent(String name, int initialBalance) throws RemoteException; // for Agents
+    public int registerAuctionHouse(String name) throws RemoteException; //this is for AuctionHouse, intial balance is always 0
     public boolean transferFunds(int senderId, int receiverId, double amount) throws RemoteException;
     public boolean unblockFunds(int accountNumber, int itemId) throws RemoteException;
     public boolean deregister(int accountNumber) throws RemoteException;
