89d21f4f052c4f02540819726ec866a1dedf858c
Colton Trujillo <coltonbtrujillo@cs.unm.edu>
2019-11-26 12:35:01 -0700

    feature: new AgentRemoteService included for Bank or AuctionHouse to specify what they will need to call on Agent

---
+++ src/Helper/AgentRemoteService.java
@@ -0,0 +1,7 @@
+package Helper;
+
+/**
+ * If you need to make calls on remote Agents this is the place to put the method signatures
+ */
+public interface AgentRemoteService extends java.rmi.Remote {
+}
