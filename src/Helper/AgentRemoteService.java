1e6cff765fd58e89b610349263d9f51352620ac2
Colton Trujillo <coltonbtrujillo@cs.unm.edu>
2019-12-02 09:54:34 -0700

    add protection from bad params  and remove proxy stuff

--- src/Helper/AgentRemoteService.java
+++ src/Helper/AgentRemoteService.java
@@ -1,5 +1,7 @@
 package Helper;
 
+import java.rmi.RemoteException;
+
 /**
  * If you need to make calls on remote Agents this is the place to put the method signatures
  */
@@ -8,5 +10,5 @@ public interface AgentRemoteService extends java.rmi.Remote {
      * Change the BidStatusMessage of the bid. This should be called when a bid's status changes in an AuctionHouse instance
      * @param bid Bid whose status is to be changed.
      */
-    public void updateBid(Bid bid);
+    public void updateBid(Bid bid) throws RemoteException;
 }
